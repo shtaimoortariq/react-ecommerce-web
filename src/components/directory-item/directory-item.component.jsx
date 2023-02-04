db3444672b2103592c9179fca26663fe6790db96
Taimoor Tariq <taimoortariqdev@gmail.com>
2023-02-04 14:21:44 +0500

    Converted Directory scss to styled component.

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,17 +1,17 @@
-import './directory-item.styles.scss';
-const DirectoryItem = ({category}) => {
-    const {imageUrl, title } = category;
-    return (
-        <div className="directory-item-container">
-            <div className='background-image' style={{
-              backgroundImage: `url(${imageUrl})`
-            }} />
-          <div className="body">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-    )
+import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles';
+const DirectoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <DirectoryItemContainer>
+      <BackgroundImage
+        imageUrl={imageUrl}
+      />
+      <Body>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </Body>
+    </DirectoryItemContainer>
+  )
 }
 
 export default DirectoryItem;

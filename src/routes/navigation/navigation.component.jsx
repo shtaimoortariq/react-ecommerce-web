349a316facb519c749036037a0fc036a1ce42bb5
shtaimoortariq <taimoortariqdev@gmail.com>
2023-03-14 22:13:45 +0500

    Add Selectors

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,10 @@
 import { Fragment, useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
+import { useSelector } from 'react-redux';
+
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import { UserContext } from '../../context/user.context';
+import { selectCurrentUser } from '../../store/user/user.selector';
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
@@ -10,8 +12,7 @@ import { CartContext } from '../../context/cart.context';
 import { NavigationContainer, LogoContainer, NavLinksContainer, NavLink } from './navigation.styles';
 
 const Navigaton = () => {
-
-    const { currentUser } = useContext(UserContext);
+    const currentUser = useSelector(selectCurrentUser);
     const { isCartOpen } = useContext(CartContext);
     return (
         <Fragment>

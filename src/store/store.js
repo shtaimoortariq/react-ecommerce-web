a070867a50658a8416fd4c0b2ef333aab1ff8511
shtaimoortariq <taimoortariqdev@gmail.com>
2023-05-28 13:48:10 +0500

    added thunk and create seprate file for store

--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,12 @@
-import {compose, legacy_createStore as createStore, applyMiddleware} from 'redux';
+import { compose, legacy_createStore as createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import {rootReducer} from './root-reducer';
+import { rootReducer } from './root-reducer';
+import thunk from 'redux-thunk';
 
-import {persistReducer, persistStore} from 'redux-persist'
+import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
-
+import { loggerMiddleware } from '../middleware/logger';
 //root reducer
-
-
-const loggerMiddleware = (store) => (next) => (action) => {
-    if(!action.type) {
-        next(action);
-    }
-
-    console.log('type :', action.type);
-    console.log('payload :', action.payload);
-    console.log('current state :', store.getState());
-
-    next(action);
-
-    console.log('next state :', store.getState());
-}
 
 const persistConfig = {
     key: 'root',
@@ -29,8 +15,12 @@ const persistConfig = {
 }
 
 const persistReducers = persistReducer(persistConfig, rootReducer);
-const middlewares = [loggerMiddleware];
-const composedEnhanser = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(persistReducers, undefined, composedEnhanser);
+const middlewares = [process.env.NODE_ENV !== 'production' && loggerMiddleware, thunk].filter(Boolean)
+
+// const middlewares = [loggerMiddleware];
+const composedEnhanser = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+const composedEnhansers = composedEnhanser(applyMiddleware(...middlewares));
+
+export const store = createStore(persistReducers, undefined, composedEnhansers);
 export const persistor = persistStore(store);
